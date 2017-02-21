5d7728d61634dc2a9c81011efc9a2738aad059b2
xcgoner <xcgoner1108@gmail.com>
2017-02-21 14:13:10 -0600

    Update __init__.py

--- autosklearn/__init__.py
+++ autosklearn/__init__.py
@@ -4,12 +4,12 @@ from autosklearn.__version__ import __version__
 
 
 __MANDATORY_PACKAGES__ = '''
-scikit-learn==0.17.1
+scikit-learn==0.18.1
 smac==0.2.2
 lockfile>=0.10
 ConfigSpace>=0.2.1
 pyrfr==0.2.0
-xgboost==0.4a30
+xgboost==0.6a2
 '''
 
 dependencies.verify_packages(__MANDATORY_PACKAGES__)
